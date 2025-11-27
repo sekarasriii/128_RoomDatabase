c24dc678e3f50eea1fdafd090565e2200c2020ca
sekarasriii <sekareeesh@gmail.com>
2025-11-27 18:39:02 +0700

    menambahkan interface yg isinya string resource id judul yg akan ditampilkan di layar halaman

--- app/src/main/java/com/example/meet7_roomdatabase/view/route/DestinasiNavigasi.kt
+++ app/src/main/java/com/example/meet7_roomdatabase/view/route/DestinasiNavigasi.kt
@@ -6,5 +6,8 @@ interface DestinasiNavigasi {
      */
     val route: String
 
-    
+    /**
+     * String resource id yang berisi judul yang akan ditampilkan di layar halaman.
+     */
+    val titleRes: Int
 }
