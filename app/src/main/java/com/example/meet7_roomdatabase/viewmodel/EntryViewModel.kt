bfb383d0f08eaf61856b23f5f66cf339ddfe8443
sekarasriii <sekareeesh@gmail.com>
2025-11-27 16:51:27 +0700

    menambahkan deklarasi data class detailsiswa yg berisi tentang variabel-variabel seperti id, nama, alamat, telepon

---
+++ app/src/main/java/com/example/meet7_roomdatabase/viewmodel/EntryViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.meet7_roomdatabase.viewmodel
+
+import com.example.meet7_roomdatabase.room.Siswa
+
+data class DetailSiswa(
+    val id: Int = 0,
+    val nama: String = "",
+    val alamat: String = "",
+    val telepon: String = "",
+)
