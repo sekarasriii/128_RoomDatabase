fbecf047ea951133403eeb9560985c7b3148833f
sekarasriii <sekareeesh@gmail.com>
2025-11-27 18:58:45 +0700

    implementasi entryviewmodel dgn state management

--- app/src/main/java/com/example/meet7_roomdatabase/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/meet7_roomdatabase/viewmodel/EntryViewModel.kt
@@ -1,41 +1,13 @@
 package com.example.meet7_roomdatabase.viewmodel
 
-import com.example.meet7_roomdatabase.room.Siswa
-import com.example.meet7_roomdatabase.repositori.RepositoriSiswa
-import androidx.lifecycle.ViewModel
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.meet7_roomdatabase.repositori.RepositoriSiswa
+import com.example.meet7_roomdatabase.room.Siswa
 
-class EntryViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel() {
+class EntryViewModel(private val repositoriSiswa: RepositoriSiswa) : ViewModel() {
     var uiStateSiswa by mutableStateOf(UIStateSiswa())
         private set
-    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa ): Boolean {
-        return with(uiState) {
-            nama.isNotBlank() && alamat.isNotBlank() && telepon.isNotBlank()
-        }
-    }
-}
-data class DetailSiswa(
-    val id: Int = 0,
-    val nama: String = "",
-    val alamat: String = "",
-    val telepon: String = "",
-)
-/*fungsi untuk mengkonversi data input ke data dlm tabel sesuai jenis datanya*/
-fun DetailSiswa.toSiswa(): Siswa = Siswa(
-    id = id,
-    nama = nama,
-    alamat = alamat,
-    telepon = telepon
-)
 
-fun Siswa.toUiStateSiswa(isEntryValid: Boolean = false): UIStateSiswa = UIStateSiswa(
-    detailSiswa = this.toDetailSiswa(),
-    isEntryValid = isEntryValid
-)
-
-fun Siswa.toDetailSiswa(): DetailSiswa = DetailSiswa(
-    id = id,
-    nama = nama,
-    alamat = alamat,
-    telepon = telepon
-)
