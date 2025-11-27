ebc14c5ae671bf93e39f01b3112bfabbe88313fe
sekarasriii <sekareeesh@gmail.com>
2025-11-27 19:13:22 +0700

    setup viewmodelfactory dgn penyediaviewmodel object

---
+++ app/src/main/java/com/example/meet7_roomdatabase/viewmodel/provider/PenyediaViewModel.kt
@@ -0,0 +1,11 @@
+package com.example.meet7_roomdatabase.viewmodel.provider
+
+import androidx.lifecycle.viewmodel.viewModelFactory
+import androidx.lifecycle.viewmodel.initializer
+import com.example.meet7_roomdatabase.viewmodel.HomeViewModel
+import com.example.meet7_roomdatabase.viewmodel.EntryViewModel
+import androidx.lifecycle.viewmodel.CreationExtras
+import  com.example.meet7_roomdatabase.repositori.AplikasiSiswa
+import androidx.lifecycle.ViewModelProvider
+
+object PenyediaViewModel {}
