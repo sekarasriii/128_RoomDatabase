8ab3b4b91d50c608d8bd18bd3d1e7a70dac26937
sekarasriii <sekareeesh@gmail.com>
2025-11-27 16:19:53 +0700

    menambahkan deklarasi class offline repositori siswa yg isinya adanya variable private siswaDao, function untuk get All siswa stream dan insert siswa

--- app/src/main/java/com/example/meet7_roomdatabase/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/meet7_roomdatabase/repositori/RepositoriSiswa.kt
@@ -1,8 +1,16 @@
 package com.example.meet7_roomdatabase.repositori
 import kotlinx.coroutines.flow.Flow
 import com.example.meet7_roomdatabase.room.Siswa
+import com.example.meet7_roomdatabase.room.SiswaDao
 
 interface RepositoriSiswa {
     fun getAllSiswaStream(): Flow<List<Siswa>>
     suspend fun insertSiswa(siswa: Siswa)
+}
+
+class OfflineRepositoriSiswa(
+    private val siswaDao: SiswaDao
+): RepositoriSiswa {
+    override fun getAllSiswaStream(): Flow<List<Siswa>> = siswaDao.getAllSiswa()
+    override suspend fun insertSiswa(siswa: Siswa) = siswaDao.insert(siswa)
 }
