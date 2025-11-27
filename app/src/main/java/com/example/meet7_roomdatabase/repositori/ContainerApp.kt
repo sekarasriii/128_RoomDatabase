baf1e2f86f8ce45bdf828f7c372f8583e2b6a7a9
sekarasriii <sekareeesh@gmail.com>
2025-11-27 16:25:59 +0700

    menambahkan deklarasi class container app

--- app/src/main/java/com/example/meet7_roomdatabase/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/meet7_roomdatabase/repositori/ContainerApp.kt
@@ -1,5 +1,17 @@
 package com.example.meet7_roomdatabase.repositori
 
+import android.content.Context
+import com.example.meet7_roomdatabase.room.DatabaseSiswa
+
 interface ContainerApp {
     val repositoriSiswa : RepositoriSiswa
 }
+
+class ContainerDataApp(private val context: Context):
+        ContainerApp {
+            override val repositoriSiswa: RepositoriSiswa by lazy {
+                OfflineRepositoriSiswa(
+                    DatabaseSiswa.getDatabase(context).siswaDao()
+                )
+            }
+        }
