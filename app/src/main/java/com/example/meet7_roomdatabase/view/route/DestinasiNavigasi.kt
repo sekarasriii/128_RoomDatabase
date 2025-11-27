4ec2fb3f5dbcdf45ed79d65bc61ab68c6bb01540
sekarasriii <sekareeesh@gmail.com>
2025-11-27 18:38:23 +0700

    menambahkan interface yg isinya nama unik untuk menentukan jalur composable

---
+++ app/src/main/java/com/example/meet7_roomdatabase/view/route/DestinasiNavigasi.kt
@@ -0,0 +1,10 @@
+package com.example.meet7_roomdatabase.view.route
+
+interface DestinasiNavigasi {
+    /**
+     * Nama unik untuk menentukan jalur untuk composable
+     */
+    val route: String
+
+    
+}
