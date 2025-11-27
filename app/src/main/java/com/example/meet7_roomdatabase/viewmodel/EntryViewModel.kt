d12cc7162635293a68cab9aa145177880f15d4b7
sekarasriii <sekareeesh@gmail.com>
2025-11-27 19:00:19 +0700

    menambahkan data classes uistatesiswa dan detailsiswa

--- app/src/main/java/com/example/meet7_roomdatabase/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/meet7_roomdatabase/viewmodel/EntryViewModel.kt
@@ -32,3 +32,15 @@ class EntryViewModel(private val repositoriSiswa: RepositoriSiswa) : ViewModel()
     }
 }
 
+data class UIStateSiswa(
+    val detailSiswa: DetailSiswa = DetailSiswa(),
+    val isEntryValid: Boolean = false
+)
+
+data class DetailSiswa(
+    val id: Int = 0,
+    val nama: String = "",
+    val alamat: String = "",
+    val telepon: String = "",
+)
+
