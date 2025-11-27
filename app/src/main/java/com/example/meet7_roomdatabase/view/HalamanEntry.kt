a8291e3c2fb347a40b2a6e89e635660ee75f19af
sekarasriii <sekareeesh@gmail.com>
2025-11-27 19:40:27 +0700

    implementasi entrysiswascreen dgn scaffold dan navigation

---
+++ app/src/main/java/com/example/meet7_roomdatabase/view/HalamanEntry.kt
@@ -0,0 +1,42 @@
+package com.example.meet7_roomdatabase.view
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.meet7_roomdatabase.R
+import com.example.meet7_roomdatabase.view.route.DestinasiEntry
+import com.example.meet7_roomdatabase.viewmodel.DetailSiswa
+import com.example.meet7_roomdatabase.viewmodel.EntryViewModel
+import com.example.meet7_roomdatabase.viewmodel.UIStateSiswa
+import com.example.meet7_roomdatabase.viewmodel.provider.PenyediaViewModel
+
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EntrySiswaScreen(
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: EntryViewModel = viewModel(factory = PenyediaViewModel.Factory)
+)
