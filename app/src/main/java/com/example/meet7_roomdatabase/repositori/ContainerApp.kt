b7a36a8a089a6eabc96ab9b99dc38f3c7894e6b7
sekarasriii <sekareeesh@gmail.com>
2025-11-27 16:34:03 +0700

    menambahkan deklarasi class aplikasi siswa yg isinya lateinit var container dan function oncreate

--- app/src/main/java/com/example/meet7_roomdatabase/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/meet7_roomdatabase/repositori/ContainerApp.kt
@@ -2,16 +2,32 @@ package com.example.meet7_roomdatabase.repositori
 
 import android.content.Context
 import com.example.meet7_roomdatabase.room.DatabaseSiswa
+import android.app.Application
 
 interface ContainerApp {
     val repositoriSiswa : RepositoriSiswa
 }
 
 class ContainerDataApp(private val context: Context):
-        ContainerApp {
-            override val repositoriSiswa: RepositoriSiswa by lazy {
-                OfflineRepositoriSiswa(
-                    DatabaseSiswa.getDatabase(context).siswaDao()
-                )
-            }
+    ContainerApp {
+        override val repositoriSiswa: RepositoriSiswa by lazy {
+            OfflineRepositoriSiswa(
+                DatabaseSiswa.getDatabase(context).siswaDao()
+            )
         }
+    }
+
+class AplikasiSiswa : Application() {
+    /**
+     * AppContainer instance digunakan oleh kelas-kelas di bawahnya untuk
+     * menyediakan dependensi
+     */
+
+
+    lateinit var container: ContainerApp
+
+    override fun onCreate() {
+        super.onCreate()
+        container = ContainerDataApp(this)
+    }
+}
