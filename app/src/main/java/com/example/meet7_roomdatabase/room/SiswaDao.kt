265e82bc38414e8fb619aeb4963ce7a16af0640d
sekarasriii <sekareeesh@gmail.com>
2025-11-27 16:02:23 +0700

    menambahkan interface class SiswaDao yg berisi @Query dan @Insert

--- app/src/main/java/com/example/meet7_roomdatabase/room/SiswaDao.kt
+++ app/src/main/java/com/example/meet7_roomdatabase/room/SiswaDao.kt
@@ -1,4 +1,15 @@
 package com.example.meet7_roomdatabase.room
+import androidx.room.Dao
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 
+@Dao
 interface SiswaDao {
+    @Query("SELECT * from tblSiswa ORDER BY nama ASC")
+    fun getAllSiswa() : Flow<List<Siswa>>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(siswa: Siswa)
 }
