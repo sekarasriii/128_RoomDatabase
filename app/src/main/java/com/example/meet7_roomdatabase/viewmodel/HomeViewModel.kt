614f967eeeb6af21a42189018f93c8e3d4323bc5
sekarasriii <sekareeesh@gmail.com>
2025-11-27 16:48:18 +0700

    menambahkan deklarasi class homeviewmodel yg berisi private variabel dan data class homeuistate

---
+++ app/src/main/java/com/example/meet7_roomdatabase/viewmodel/HomeViewModel.kt
@@ -0,0 +1,26 @@
+package com.example.meet7_roomdatabase.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.meet7_roomdatabase.repositori.RepositoriSiswa
+import com.example.meet7_roomdatabase.room.Siswa
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+
+class HomeViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel() {
+    companion object {
+        private const val TIMEOUT_MILLIS = 5_000L
+    }
+
+    val homeUiState: StateFlow<HomeUiState> = repositoriSiswa.getAllSiswaStream().filterNotNull()
+        .map {HomeUiState(listSiswa = it.toList())}
+        .stateIn(scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
+            initialValue = HomeUiState())
+    data class HomeUiState(
+        val listSiswa: List<Siswa> = listOf()
+    )
+}
