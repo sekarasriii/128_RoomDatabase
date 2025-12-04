63ca6eaeb7af494e35c5f7e750e6009c8221e5ae
sekarasriii <sekareeesh@gmail.com>
2025-12-04 15:50:15 +0700

    menambahkan override delete dan update data siswa di class offline repositori siswa

--- app/src/main/java/com/example/meet7_roomdatabase/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/meet7_roomdatabase/repositori/RepositoriSiswa.kt
@@ -17,4 +17,8 @@ class OfflineRepositoriSiswa(
 ): RepositoriSiswa {
     override fun getAllSiswaStream(): Flow<List<Siswa>> = siswaDao.getAllSiswa()
     override suspend fun insertSiswa(siswa: Siswa) = siswaDao.insert(siswa)
+    //edit 2 : tambah 3 override berikut
+    override fun getSiswaStream(id: Int): Flow<Siswa?> = siswaDao.getSiswa(id)
+    override suspend fun deleteSiswa(siswa: Siswa) = siswaDao.delete(siswa)
+    override suspend fun updateSiswa(siswa: Siswa) = siswaDao.update(siswa)
 }
