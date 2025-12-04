cbffe307c30bbf1fc632919f4e4083ba146d8826
sekarasriii <sekareeesh@gmail.com>
2025-12-04 15:23:54 +0700

    menambahkan aturan scaffold untuk tampilan detail siswa

--- app/src/main/java/com/example/meet7_roomdatabase/view/HalamanDetailSiswa.kt
+++ app/src/main/java/com/example/meet7_roomdatabase/view/HalamanDetailSiswa.kt
@@ -1,11 +1,23 @@
 package com.example.meet7_roomdatabase.view
 
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.meet7_roomdatabase.viewmodel.DetailViewModel
 import  com.example.meet7_roomdatabase.viewmodel.provider.PenyediaViewModel
+import androidx.compose.ui.res.stringResource
+import com.example.meet7_roomdatabase.view.route.DestinasiDetailSiswa
+import androidx.compose.runtime.collectAsState
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.res.dimensionResource
+import com.example.meet7_roomdatabase.R
+import androidx.compose.material3.Icon
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -14,4 +26,20 @@ fun DetailSiswaScreen(
     navigateBack: () -> Unit,
     modifier: Modifier = Modifier,
     viewModel: DetailViewModel = viewModel(factory = PenyediaViewModel.Factory)
-)
+){Scaffold(
+    topBar = {
+        SiswaTopAppBar(
+            title = stringResource(DestinasiDetailSiswa.titleRes),
+            canNavigateBack = true,
+            navigateUp = navigateBack
+        )
+    },
+    floatingActionButton = {
+        val uiState = viewModel.uiDetailState.collectAsState()
+        FloatingActionButton(
+            onClick = { navigateToEditItem(uiState.value.detailSiswa.id) },
+            shape = MaterialTheme.shapes.medium,
+            modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_large))
+
+        )
+)}
