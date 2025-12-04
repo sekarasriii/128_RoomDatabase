202ddc962fbb6c67a50d0daa66fab644f778e6a3
sekarasriii <sekareeesh@gmail.com>
2025-12-04 15:19:41 +0700

    menambahkan deklarasi navigasi untuk eidt dan back halaman di fungsi detail siswa screen

---
+++ app/src/main/java/com/example/meet7_roomdatabase/view/HalamanDetailSiswa.kt
@@ -0,0 +1,17 @@
+package com.example.meet7_roomdatabase.view
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.meet7_roomdatabase.viewmodel.DetailViewModel
+import  com.example.meet7_roomdatabase.viewmodel.provider.PenyediaViewModel
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DetailSiswaScreen(
+    navigateToEditItem: (Int) -> Unit,
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: DetailViewModel = viewModel(factory = PenyediaViewModel.Factory)
+)
