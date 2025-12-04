53c25c5a3f0df7b197242540e5fd6948d945fcda
sekarasriii <sekareeesh@gmail.com>
2025-12-04 15:49:12 +0700

    menambahkan fungsi delete dan update data siswa di interface repositori siswa

--- app/src/main/java/com/example/meet7_roomdatabase/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/meet7_roomdatabase/repositori/RepositoriSiswa.kt
@@ -6,6 +6,10 @@ import com.example.meet7_roomdatabase.room.SiswaDao
 interface RepositoriSiswa {
     fun getAllSiswaStream(): Flow<List<Siswa>>
     suspend fun insertSiswa(siswa: Siswa)
+    //edit 1 : tambah 3 fungsi berikut
+    fun getSiswaStream(id: Int): Flow<Siswa?>
+    suspend fun deleteSiswa(siswa: Siswa)
+    suspend fun updateSiswa(siswa: Siswa)
 }
 
 class OfflineRepositoriSiswa(
