9f94fd0fa6b4db6f5ffc8be44f5e2318c6a9000f
sekarasriii <sekareeesh@gmail.com>
2025-12-04 15:11:11 +0700

    menambahkan private variable id siswa sbg primary key data yg akan ditampilkan di detail siswa

--- app/src/main/java/com/example/meet7_roomdatabase/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/meet7_roomdatabase/viewmodel/DetailViewModel.kt
@@ -3,7 +3,9 @@ package com.example.meet7_roomdatabase.viewmodel
 import androidx.lifecycle.SavedStateHandle
 import com.example.meet7_roomdatabase.repositori.RepositoriSiswa
 import androidx.lifecycle.ViewModel
+import com.example.meet7_roomdatabase.view.route.DestinasiDetailSiswa
 
 class DetailViewModel (
     savedStateHandle: SavedStateHandle,
-    private val repositoriSiswa: RepositoriSiswa) : ViewModel()
+    private val repositoriSiswa: RepositoriSiswa) : ViewModel() {
+    private val idSiswa: Int = checkNotNull(savedStateHandle[DestinasiDetailSiswa.itemIdArg])}
