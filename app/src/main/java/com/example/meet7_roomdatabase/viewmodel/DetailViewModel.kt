5efa0824f5554f34d794fbc3155700aa32661312
sekarasriii <sekareeesh@gmail.com>
2025-12-04 15:15:07 +0700

    menambahkan fungsi untuk menampilkan data dan menghapus data siswa

--- app/src/main/java/com/example/meet7_roomdatabase/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/meet7_roomdatabase/viewmodel/DetailViewModel.kt
@@ -4,8 +4,24 @@ import androidx.lifecycle.SavedStateHandle
 import com.example.meet7_roomdatabase.repositori.RepositoriSiswa
 import androidx.lifecycle.ViewModel
 import com.example.meet7_roomdatabase.view.route.DestinasiDetailSiswa
+import kotlinx.coroutines.flow.StateFlow
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.SharingStarted
 
 class DetailViewModel (
     savedStateHandle: SavedStateHandle,
     private val repositoriSiswa: RepositoriSiswa) : ViewModel() {
-    private val idSiswa: Int = checkNotNull(savedStateHandle[DestinasiDetailSiswa.itemIdArg])}
+    private val idSiswa: Int = checkNotNull(savedStateHandle[DestinasiDetailSiswa.itemIdArg])
+    val uiDetailState: StateFlow<DetailSiswaUiState> =
+        repositoriSiswa.getSiswaStream(idSiswa)
+            .filterNotNull()
+            .map {
+                DetailSiswaUiState(detailSiswa = it.toDetailSiswa())
+            }.stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
+                initialValue = DetailSiswaUiState()
+            )
+    suspend fun deleteSiswa(){
+        repositoriSiswa.deleteSiswa(uiDetailState.value.detailSiswa.toSiswa())
+    }
