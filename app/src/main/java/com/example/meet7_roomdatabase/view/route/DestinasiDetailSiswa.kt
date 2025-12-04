742ab522a4798532237cac6de50bac9bd984eec6
sekarasriii <sekareeesh@gmail.com>
2025-12-04 15:04:56 +0700

    menambahkan object destinasi detail siswa di folder route untuk navigasi

---
+++ app/src/main/java/com/example/meet7_roomdatabase/view/route/DestinasiDetailSiswa.kt
@@ -0,0 +1,9 @@
+package com.example.meet7_roomdatabase.view.route
+import com.example.meet7_roomdatabase.R
+
+object DestinasiDetailSiswa : DestinasiNavigasi {
+    override val route = "detail_siswa"
+    override val titleRes = R.string.detail_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
