e3f05e5318e46dff645470256fe200c9915c146b
sekarasriii <sekareeesh@gmail.com>
2025-12-04 15:16:05 +0700

    menambahkan deklarasi companion object untuk variable timeout millis

--- app/src/main/java/com/example/meet7_roomdatabase/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/meet7_roomdatabase/viewmodel/DetailViewModel.kt
@@ -25,3 +25,8 @@ class DetailViewModel (
     suspend fun deleteSiswa(){
         repositoriSiswa.deleteSiswa(uiDetailState.value.detailSiswa.toSiswa())
     }
+
+    companion object {
+        private const val TIMEOUT_MILLIS = 5_000L
+    }}
+
