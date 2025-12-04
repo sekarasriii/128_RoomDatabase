76c3f572ee07f5b6485a33ada980886f86503dac
sekarasriii <sekareeesh@gmail.com>
2025-12-04 15:31:09 +0700

    revisi ulang code menambahkan simbol command code sementara

--- app/src/main/java/com/example/meet7_roomdatabase/view/HalamanDetailSiswa.kt
+++ app/src/main/java/com/example/meet7_roomdatabase/view/HalamanDetailSiswa.kt
@@ -22,6 +22,7 @@ import android.R.attr.shape
 import androidx.compose.runtime.rememberCoroutineScope
 import kotlinx.coroutines.launch
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -30,40 +31,41 @@ fun DetailSiswaScreen(
     navigateBack: () -> Unit,
     modifier: Modifier = Modifier,
     viewModel: DetailViewModel = viewModel(factory = PenyediaViewModel.Factory)
-){Scaffold(
-    topBar = {
-        SiswaTopAppBar(
-            title = stringResource(DestinasiDetailSiswa.titleRes),
-            canNavigateBack = true,
-            navigateUp = navigateBack
-        )
-    },
-    floatingActionButton = {
-        val uiState = viewModel.uiDetailState.collectAsState()
-        FloatingActionButton(
-            onClick = { //navigateToEditItem(uiState.value.detailSiswa.id) },
-            shape = MaterialTheme.shapes.medium,
-            modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_large))
-
-        ){
-            Icon(
-                imageVector = Icons.Default.Edit,
-                contentDescription = stringResource(R.string.update),
+){
+    Scaffold(
+        topBar = {
+            SiswaTopAppBar(
+                title = stringResource(DestinasiDetailSiswa.titleRes),
+                canNavigateBack = true,
+                navigateUp = navigateBack
             )
-        }
-    }, modifier = modifier
-){ innerPadding ->
+        },
+        floatingActionButton = {
             val uiState = viewModel.uiDetailState.collectAsState()
-            val coroutineScope = rememberCoroutineScope()
-            BodyDetailDataSiswa(
-                detailSiswaUiState = uiState.value,
-                onDelete = { coroutineScope.launch {
-                    viewModel.deleteSiswa()
-                    navigateBack()
-                }},
-                modifier = Modifier
-                    .padding(innerPadding)
-                    .verticalScroll(rememberScrollState())
-            )
-        }
+            FloatingActionButton(
+                onClick = //{ navigateToEditItem(uiState.value.detailSiswa.id) },
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_large))
+
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Edit,
+                    contentDescription = stringResource(R.string.update),
+                )
+            }
+        }, modifier = modifier
+    ) { innerPadding ->
+        val uiState = viewModel.uiDetailState.collectAsState()
+        val coroutineScope = rememberCoroutineScope()
+        BodyDetailDataSiswa(
+            detailSiswaUiState = uiState.value,
+            onDelete = { coroutineScope.launch {
+                viewModel.deleteSiswa()
+                navigateBack()
+            }},
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+        )
     }
+}
